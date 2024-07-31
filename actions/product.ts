45a8e6fb11c7b6aca591186c7017a89512bfeb24
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2024-07-31 17:30:18 +0100

    added new function

--- actions/product.ts
+++ actions/product.ts
@@ -69,6 +69,7 @@ const getAllProducts = async () => {
     try {
         const products = await db.product.findMany({});
         return products;
+
     } catch (error) {
         console.error("Failed to fetch products:", error);
         throw new Error("Failed to fetch products");
--- components/homepage/products.tsx
+++ components/homepage/products.tsx
@@ -1,9 +1,26 @@
+"use client"
 import { ProductCard } from "../products/ProductCard"
-import { ProductTypes } from "@/types"
+import { ProductCardProps, ProductTypes } from "@/types"
 import { getAllProducts } from "@/actions/product"
+import { useEffect, useState } from "react"
+import toast from "react-hot-toast"
 
-export const Products = async() => {
-    const products = await getAllProducts()
+export const Products = () => {
+    const [products, setProducts] = useState<ProductCardProps[]>([]);
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const allProducts = await getAllProducts();
+                setProducts(allProducts);
+            } catch (error) {
+                console.error("Failed to fetch products:", error);
+                toast.error("Failed to load products");
+            }
+        };
+
+        fetchProducts();
+    }, []);
+   
     return (
         <div className="my-10 px-4 md:px-6 lg:px-8 flex justify-center">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 items-center justify-center">
@@ -14,7 +31,6 @@ export const Products = async() => {
                         name={product.name}
                         price={product.price}
                         id={product.id}
-                        // description={product.description}
                     />
                 ))}
             </div> 
